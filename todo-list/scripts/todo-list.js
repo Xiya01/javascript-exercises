fa29b8efc6b6b4b94601dde2ea369a6e1dc6f94a
Xiya01 <sylwia.baranska@pm.me>
2024-08-07 15:47:43 +0200

    added data and delete button

--- todo-list/scripts/todo-list.js
+++ todo-list/scripts/todo-list.js
@@ -4,7 +4,13 @@
 // 3. get text from textbox
 // 4. add it to array
 
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-23'
+}];
 
 renderTodoList();
 
@@ -13,8 +19,19 @@ function renderTodoList() {
   let todoListHTML = '';
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const { name, dueDate } = todoObject;
+    const html = `
+      <p>
+        ${name} ${dueDate}
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        ">Delete</button>
+      </p>
+    `;
     todoListHTML += html;
   }
 
@@ -28,8 +45,15 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate
+    name,
+    dueDate
+  });
 
   inputElement.value = '';
 
